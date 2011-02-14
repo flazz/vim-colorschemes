2dad8fdcfd6e80cb1d2a37f8f5ab6abd7d2832b0
Francesco Lazzarino <flazzarino@gmail.com>
2011-02-14 14:22:34 -0500

    script for all colorschemes on vim.org

---
+++ harvest.rb
@@ -0,0 +1,45 @@
+require 'nokogiri'
+require 'open-uri'
+require 'yaml'
+require 'curb'
+require 'ruby-debug'
+
+# URL = 'http://www.vim.org/scripts/script_search_results.php?&script_type=color%20scheme&show_me=1000'
+
+URL = 'http://www.vim.org/scripts/'
+FILE = 'links'
+
+def get_index
+  doc = Nokogiri::HTML open(URL + 'script_search_results.php?&script_type=color%20scheme&show_me=1000')
+  as = doc / '//tr[td[position()=2] = "color scheme"]/td[position()=1]/a'
+  as.map { |a| URL + a['href'] }
+end
+
+def get_script_links
+  YAML.load_file FILE
+rescue Errno::ENOENT => e
+  page_urls = get_index
+  downloads = []
+
+  Curl::Multi.get(page_urls, {:follow_location => true}, {:pipeline => true}) do |easy|
+    script_doc = Nokogiri::HTML easy.body_str
+    a = script_doc % "//tr/td/a[starts-with(@href, 'download_script.php?src_id=')]"
+
+    if a
+      url = URL + a['href']
+      name = a.text
+      downloads << [name, url]
+    end
+
+  end
+
+  open(FILE, 'w') { |io| YAML.dump downloads, io }
+  downloads
+  retry
+end
+
+names, urls = get_script_links.transpose
+
+Dir.chdir 'colors' do
+  Curl::Multi.download(urls, {:follow_location => true}, {:pipeline => true}, names)
+end
