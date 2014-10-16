b3c53d0d1521b1ee08dcbbff355a9e44179af4b5
Kurt Preston <bitbucket@kurtpreston.com>
2014-10-15 23:47:41 -0500

    Adding vimcolor theme retrieval script

---
+++ retrieve_vimcolor_themes.rb
@@ -0,0 +1,27 @@
+require 'json'
+require 'open-uri'
+require 'fileutils'
+
+base_url = "http://vimcolors.com/api/colorschemes"
+
+FileUtils.mkdir_p "repos"
+Dir.chdir "repos"
+
+# Get up to 20 pages of colorschemes
+(1..20).each do |i|
+  json = JSON.parse(open("#{base_url}?page=#{i}").read)
+
+  # Quit if at end of the list
+  break if json["colorschemes"].nil?
+
+  # For each colorscheme, clone the git repo
+  json["colorschemes"].each do |colorscheme|
+    github_repo = colorscheme["github_repo"]["address"]
+    system("git clone #{github_repo}")
+  end
+  sleep 1
+end
+
+# Copy files into the colors directory
+Dir.chdir ".."
+system("cp repos/*/colors/*.vim colors/")
