092cc0a77a4630dce2b6b53b22fd71547f7dbf44
Kurt Preston <kpreston@opinionlab.com>
2015-12-14 16:45:15 -0600

    Handling invalid git repos

--- retrieve_vimcolor_themes.rb
+++ retrieve_vimcolor_themes.rb
@@ -8,6 +8,19 @@ base_url = "http://vimcolors.com/api/colorschemes"
 FileUtils.mkdir_p "repos"
 Dir.chdir "repos"
 
+def clone_repo(github_path)
+  pid = Process.spawn("git clone git@github.com:#{github_path}")
+
+  begin
+    Timeout.timeout(15) do
+      Process.wait(pid)
+    end
+  rescue Timeout::Error
+    puts 'Canceling clone, taking too long'
+    Process.kill('TERM', pid)
+  end
+end
+
 # Get up to 20 pages of colorschemes
 (1..20).each do |i|
   json = JSON.parse(open("#{base_url}?page=#{i}").read)
@@ -18,8 +31,12 @@ Dir.chdir "repos"
   # For each colorscheme, clone the git repo
   json["colorschemes"].each do |colorscheme|
     github_repo = colorscheme["github_repo"]["address"]
+    begin
     uri = URI(github_repo)
-    system("git clone git@github.com:#{uri.path}")
+    clone_repo(uri.path)
+    rescue URI::InvalidURIError
+      # Ignore poorly formatted URIs. Go to next.
+    end
   end
   sleep 1
 end
